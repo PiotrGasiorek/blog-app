59505711fa853a6658034b276cfaabb0b974db3e
Piotr <piotrjgasiorek@gmail.com>
2021-11-22 16:36:06 +0100

    Created nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <img alt="blogdog logo" src="../assets/logo.svg">
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
