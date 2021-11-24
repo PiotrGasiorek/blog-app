188d0dc0aaa9aa28e4f0138ca46c4079c5a23470
Piotr <piotrjgasiorek@gmail.com>
2021-11-24 09:53:59 +0100

    Created pages for router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Nav/>
+  <PostsList/>
+</template>
+
+<script>
+import PostsList from '../components/PostsList.vue';
+import Nav from '../components/Nav.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    PostsList,
+    Nav
+  }
+}
+</script>
---
+++ src/views/Post.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <img src="" alt="">
+    </div>
+    <div>
+      <PostAuthor v-bind:author="post.author"/>
+      <small>{{ post.date }}</small>
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.paragraph }}</p>
+    </div>
+</template>
+
+<script>
+import PostAuthor from '../components/PostAuthor.vue'
+
+export default {
+  name: 'PostPage',
+  props: ['post'],
+  components: {
+    PostAuthor
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
