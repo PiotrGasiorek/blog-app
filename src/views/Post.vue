6e47c6240c10113091d2481f7189f032d60bbfd9
Piotr <piotrjgasiorek@gmail.com>
2021-11-25 17:58:37 +0100

    Added fix to prevent displaying previous post when new one is loaded

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 v-if='this.$store.state.loading'>Loading post</h1>
-  <div v-else v-for="post in this.$store.state.currentPost" v-bind:key="post.id">
+  <div v-else-if="this.$route.params.id == this.$store.state.currentPost[0].id" v-for="post in this.$store.state.currentPost" v-bind:key="post.id">
     <h1>{{ post.title }}</h1>
     <small>{{ post.date }}</small>
     <PostAuthor v-bind:author="post.author"/>
