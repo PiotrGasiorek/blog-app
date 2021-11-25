864dde0641f34d71bd863c1e46f3ca5c030d3093
Piotr <piotrjgasiorek@gmail.com>
2021-11-25 17:41:14 +0100

    Connected router with single post page

--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,16 @@ import { createStore } from 'vuex'
 const store = createStore({
     state: {
         posts: [],
+        currentPost: {},
+        loading: true,
         api: {
-            href: 'https://my-json-server.typicode.com/PiotrGasiorek/blog-app/posts'
+            href: 'https://my-json-server.typicode.com/PiotrGasiorek/blog-app/posts/'
         }
     },
 
     mutations: {
         deletePost: (state, post) => {
-            fetch(`${state.api.href}${post.id}`, {
+            fetch(state.api.href + post.id, {
                 method: 'DELETE',
                 headers: {
                   'Content-type': 'application/json; charset=UTF-8'
@@ -19,13 +21,28 @@ const store = createStore({
             state.posts.splice(state.posts.indexOf(post), 1);
         },
 
-        setPosts: state => {
+        getBlog: state => {
             fetch(state.api.href)
               .then((response) => response.json())
               .then((response) => {
                 state.posts = response;
               })
               .catch(error => console.log(error));
+        },
+
+        getPost: (state, id) => {
+            let noPosts = !state.posts;
+            let noSuchPost = state.posts.filter(post => post.id === id).length === 0;
+
+            if(noPosts || noSuchPost || state.currentPost){
+                fetch(state.api.href + id)
+                    .then((response) => response.json())
+                    .then((response) => {
+                        state.currentPost = [response];
+                        state.loading = false;
+                    })
+                    .catch(error => console.log(error));
+            }
         },
     },
 
@@ -34,8 +51,12 @@ const store = createStore({
             context.commit('deletePost', post);
         },
 
-        setPosts: (context) => {
-            context.commit('setPosts');
+        getBlog: context => {
+            context.commit('getBlog');
+        },
+
+        getPost: (context, id) => {
+            context.commit('getPost', id);
         },
     },
 
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,23 +1,31 @@
 <template>
-    <div>
-      <img src="" alt="">
-    </div>
-    <div>
-      <PostAuthor v-bind:author="post.author"/>
-      <small>{{ post.date }}</small>
-      <h1>{{ post.title }}</h1>
-      <p>{{ post.paragraph }}</p>
-    </div>
+  <h1 v-if='this.$store.state.loading'>Loading post</h1>
+  <div v-else v-for="post in this.$store.state.currentPost" v-bind:key="post.id">
+    <h1>{{ post.title }}</h1>
+    <small>{{ post.date }}</small>
+    <PostAuthor v-bind:author="post.author"/>
+    <p>{{ post.paragraph }}</p>
+    <img src="" alt="">
+  </div>
 </template>
 
 <script>
-import PostAuthor from '../components/PostAuthor.vue'
+import PostAuthor from '../components/PostAuthor.vue';
 
 export default {
   name: 'PostPage',
-  props: ['post'],
   components: {
     PostAuthor
+  },
+
+  methods: {
+    getPost(id) {
+      return this.$store.dispatch('getPost', id);
+    },
+  }, 
+
+  mounted() {
+    this.getPost(this.$route.params.id);
   },
 }
 </script>
